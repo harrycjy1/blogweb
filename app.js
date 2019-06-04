ef2bf94f4611ef54242c5976ceaa9c948145acf5
choddol <harrycjy123@gmail.com>
2019-06-04 11:27:38 +0900

    deleting, writing

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "This is for my portfolio have fun ^.^ ";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(express.static("public"));
 //db연결
-mongoose.connect("mongodb+srv://admin-cho:test123@cluster0-isdzk.mongodb.net/blogDB", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb+srv://admin-cho:test123@cluster0-isdzk.mongodb.net/blogDB",
+  {
+    useNewUrlParser: true
+  }
+);
 
 const postSchema = {
   title: String,
@@ -29,9 +36,7 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
-
 app.get("/", function(req, res) {
-
   //postDB에 posts collections에서 post를 모두 찾아 array로 반환
   Post.find({}, function(err, posts) {
     res.render("home", {
@@ -39,7 +44,6 @@ app.get("/", function(req, res) {
       posts: posts
     });
   });
-
 });
 app.get("/compose", function(req, res) {
   res.render("compose");
@@ -57,14 +61,13 @@ app.get("/contact", function(req, res) {
   });
 });
 
-
-
 app.post("/compose", function(req, res) {
+  //객체 생성 후
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
-
+  //몽고 디비에 저장
   post.save(function(err) {
     if (!err) {
       res.redirect("/");
@@ -73,22 +76,33 @@ app.post("/compose", function(req, res) {
 });
 
 app.get("/posts/:postId", function(req, res) {
-
-
   const requestedPostId = req.params.postId;
 
-  Post.findOne({
-    _id: requestedPostId
-  }, function(err, post) {
-    if (!err) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
+  Post.findOne(
+    {
+      _id: requestedPostId
+    },
+    function(err, post) {
+      if (!err) {
+        res.render("post", {
+          title: post.title,
+          content: post.content
+        });
+      }
     }
-  });
+  );
+});
 
+app.post("/delete/:postId", (req, res) => {
+  const requestedPostId = req.params.postId;
 
+  try {
+    Post.deleteOne({ _id: requestedPostId }, err => {
+      if (!err) {
+        res.redirect("/");
+      }
+    });
+  } catch (error) {}
 });
 
 app.listen(process.env.PORT || 3000, function() {
